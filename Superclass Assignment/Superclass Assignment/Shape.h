1dc670ed9ca969eec204e26d99b8b71cfadc4abb
Urist-McDev <102614832+Urist-McDev@users.noreply.github.com>
2023-09-22 11:44:46 -0400

    Superclass assignment

---
+++ Superclass Assignment/Superclass Assignment/Shape.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+class Shape
+{
+public:
+	string color;
+	void getArea() {
+		cout << "The shape has an area alright";
+	}
+};
+
